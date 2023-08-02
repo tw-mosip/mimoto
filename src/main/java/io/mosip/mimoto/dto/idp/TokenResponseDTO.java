822ae261fa6c0fb25a5ca2d349cd82dcd348d224
Vijay <vijay@Vijays-MacBook-Pro.local>
2023-08-02 14:51:25 +0530

    feat(INJI-221): [Vijay] modify the getToken endpoint to support oidc flow

--- src/main/java/io/mosip/mimoto/dto/idp/TokenResponseDTO.java
+++ src/main/java/io/mosip/mimoto/dto/idp/TokenResponseDTO.java
@@ -8,10 +8,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @Data
+
 public class TokenResponseDTO {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String id_token;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String token_type;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String access_token;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private int expires_in;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String scope;
 }
