7460ee37e2cdc378592f1e82d32540f77590a55f
Shiva <shivakumar@beehyv.com>
2024-02-23 13:58:26 +0530

    Passing Display name to the Header in the IssuerPage Signed-off-by: Shiva <shivakumar@beehyv.com>

--- inji-web/src/pages/Home/IssuerList.js
+++ inji-web/src/pages/Home/IssuerList.js
@@ -27,7 +27,7 @@ const getCardsData = (issuersList, navigate) => {
             title: issuer.display[0].name,
             icon: null,
             onClick: () => {
-                navigate(`/issuers/${issuer.credential_issuer}`)
+                navigate(`/issuers/${issuer.credential_issuer}`, {state: {issuerDisplayName: issuer.display[0].name, clientId: issuer.client_id}})
             },
             clickable: true
         }
--- inji-web/src/pages/IssuerPage/index.js
+++ inji-web/src/pages/IssuerPage/index.js
@@ -4,13 +4,16 @@ import Header from "./Header";
 import CertificateList from "./CertificateList";
 import _axios from 'axios';
 import {getCredentialsSupportedUrl, getSearchIssuersUrl} from "../../utils/config";
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 function Issuer() {
     const { issuerId } = useParams();
     const [credentialsList, setCredentialsList] = useState([]);
     const [defaultList, setDefaultList] = useState([]);
-    const [issuerClientId, setIssuerClientId] = useState();
+
+    const location = useLocation();
+    const [issuerClientId, setIssuerClientId] = useState(location.state ? location.state['clientId'] : undefined);
+    const [issuerDisplayName, setIssuerDisplayName] = useState(location.state ? location.state['issuerDisplayName'] : undefined);
     const [loading, setLoading] = useState(true);
     const [errorMessage, setErrorMessage] = useState();
 
@@ -31,23 +34,27 @@ function Issuer() {
     }, []);
 
     useEffect(() => {
+        // make an api call only when the state is not available
+        if (issuerClientId && issuerDisplayName) return;
         _axios.get(getSearchIssuersUrl(issuerId))
             .then((response) => {
                 let issuers =  response.data.response.issuers;
                 if (issuers.length !== 0) {
                     setIssuerClientId(issuers[0].client_id);
+                    setIssuerDisplayName(issuers[0].display[0].name);
                 }
             })
             .catch((error) => {
                 console.error('Failed to search for the issuer', issuerId, error);
-                setErrorMessage('Error loading issuer client id');
+                setErrorMessage('Error loading issuer details!');
             })
     }, []);
 
     // TODO: show a loader while loading and error message in case of any errors
     return (
         <PageTemplate>
-            <Header issuer={issuerId} credentialsList={credentialsList} updateCredentialsList={setCredentialsList} defaultList={defaultList}/>
+            <Header issuerDisplayName={issuerDisplayName} credentialsList={credentialsList}
+                    loading={loading} updateCredentialsList={setCredentialsList} defaultList={defaultList}/>
             <CertificateList credentialList={credentialsList} issuerId={issuerId} clientId={issuerClientId}/>
         </PageTemplate>
     );
