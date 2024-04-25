c744bb26bc206d9ac5fd6af02b31482f421728a2
abhishek8shankar <127825992+abhishek8shankar@users.noreply.github.com>
2024-04-25 13:55:30 +0530

    [DSD-5029] Update install.sh
    Signed-off-by: abhishek8shankar <127825992+abhishek8shankar@users.noreply.github.com>

--- helm/inji-web/install.sh
+++ helm/inji-web/install.sh
@@ -9,42 +9,38 @@ fi
 NS=injiweb
 CHART_VERSION=0.0.1-develop
 
+DEFAULT_MOSIP_INJIWEB_HOST=$( kubectl get cm global -n config-server -o jsonpath={.data.mosip-injiweb-host} )
+# Check if MOSIP_INJIWEB_HOST is present under configmap/global of configserver
+if echo "$DEFAULT_MOSIP_INJIWEB_HOST" | grep -q "MOSIP_INJIWEB_HOST"; then
+    echo "MOSIP_INJIWEB_HOST is already present in configmap/global of configserver"
+    MOSIP_INJIWEB_HOST=DEFAULT_MOSIP_INJIWEB_HOST
+else
+    read -p "Please provide injiwebhost (eg: injiweb.sandbox.xyz.net ) : " MOSIP_INJIWEB_HOST
 
-read -p "Please provide injiwebhost (eg: injiweb.sandbox.xyz.net ) : " MOSIP_INIEB_HOST
+    if [ -z "MOSIP_INJIWEB_HOST" ]; then
+    echo "INJIWEB Host not provided; EXITING;"
+    exit 0;
+    fi    
+fi   
 
-if [ -z "MOSIP_INIEB_HOST" ]; then
-  echo "INJIWEB Host not provided; EXITING;"
-  exit 0;
+CHK_MOSIP_INJIWEB_HOST=$( nslookup "$MOSIP_INJIWEB_HOST" )
+if [ $? -gt 0 ]; then
+    echo "Injiweb Host does not exists; EXITING;"
+    exit 0;
 fi
 
-# Check if MOSIP_INJIWEB_HOST is present under configmap/global of configserver
-if kubectl get cm global -n config-server -o jsonpath={.data.mosip-injiweb-host} | grep -q "MOSIP_INJIWEB_HOST"; then
-    echo "MOSIP_INJIWEB_HOST is already present in configmap/global of configserver"
-else
-    echo "MOSIP_INJIWEB_HOST is not present in configmap/global of configserver"
+echo "MOSIP_INJIWEB_HOST is not present in configmap/global of configserver"
     # Add injiweb host to global
-    kubectl patch configmap global --type merge -p "{\"data\": {\"mosip-injiweb-host\": \"$MOSIP_INIEB_HOST\"}}"
+    kubectl patch configmap global -n config-server --type merge -p "{\"data\": {\"mosip-injiweb-host\": \"$MOSIP_INIEB_HOST\"}}"
     # Add the host
     kubectl set env deployment/config-server SPRING_CLOUD_CONFIG_SERVER_OVERRIDES_MOSIP_ESIGNET_INJIWEB_HOST=$MOSIP_INJIWEB_HOST -n config-server
     # Restart the configserver deployment
-    kubectl -n config-server get deploy -o name | xargs -n1 -t kubectl -n config-server rollout restart
-fi
+    kubectl -n config-server get deploy -o name | xargs -n1 -t kubectl -n config-server rollout restart 
 
+sleep 400s
 
 echo Create $NS namespace
 kubectl create ns $NS
-
-function ensure_injiweb_host() {
-  # Check if mosip-injiweb-host is present in global config map of config-server
-  if ! kubectl get cm config-server -n $NS -o jsonpath='{.data.mosip-esignet-host}' | grep -q 'mosip-injiweb-host'; then
-    echo "Adding mosip-injiweb-host to config-server global config map"
-    kubectl patch configmap config-server -n $NS --type json -p '[{"op": "add", "path": "/data/mosip-injiweb-host", "value": "injiweb.sandbox.xyz.net"}]'
-    # Restart config-server
-    kubectl rollout restart deployment config-server -n $NS
-  else
-    echo "mosip-injiweb-host already present in config-server global config map"
-  fi
-}
 
 function installing_inji-web() {
   echo Istio label
@@ -56,12 +52,10 @@ function installing_inji-web() {
   echo Copy configmaps
   ./copy_cm.sh
 
-  ESIGNET_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-esignet-host})
   INJI_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-injiweb-host})
   echo Installing INJIWEB
   helm -n $NS install injiweb mosip/inji-web \
   -f values.yaml \
-  --set esignet_redirect_url=$ESIGNET_HOST \
   --set istio.hosts\[0\]=$INJI_HOST \
   --version $CHART_VERSION
 
