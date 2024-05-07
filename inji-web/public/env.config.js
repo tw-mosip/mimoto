8ef21298b632aebfc558384301e33d8aa79213df
Vijay Kumar S <94220135+vijay151096@users.noreply.github.com>
2024-05-07 11:39:08 +0530

    [INJIWEB-257]: Modify the Default Theme
    Signed-off-by: Vijay Kumar S <94220135+vijay151096@users.noreply.github.com>

--- inji-web/public/env.config.js
+++ inji-web/public/env.config.js
@@ -1,6 +1,6 @@
 window._env_ = {
     DEFAULT_LANG: "en",
-    DEFAULT_THEME: "purple_theme",
+    DEFAULT_THEME: "",
     DEFAULT_FAVICON: "favicon.ico",
     DEFAULT_TITLE: "Inji Web",
     DEFAULT_FONT_URL: "https://fonts.googleapis.com/css?family=Inter"
