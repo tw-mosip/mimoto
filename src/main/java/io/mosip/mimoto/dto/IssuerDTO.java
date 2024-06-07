6d0f6e13826daa53b35002c9e629cf757fdc729c
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-06-07 15:00:10 +0530

    [INJIMOB-1458] add supported credentials attribute in issuers config (#244)
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/dto/IssuerDTO.java
+++ src/main/java/io/mosip/mimoto/dto/IssuerDTO.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.Map;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 
 @Data
@@ -46,27 +47,37 @@ public class IssuerDTO {
     @NotBlank
     String authorization_audience;
     @JsonInclude(NON_NULL)
-    @NotBlank
     String token_endpoint;
     @JsonInclude(NON_NULL)
     @NotBlank
     String proxy_token_endpoint;
     @JsonInclude(NON_NULL)
-    @NotBlank
     String credential_endpoint;
     @JsonInclude(NON_NULL)
     @NotEmpty
     List<String> credential_type;
     @JsonInclude(NON_NULL)
-    @NotBlank
     String credential_audience;
     @JsonInclude(NON_NULL)
     @NotBlank
     String client_alias;
     @JsonInclude(NON_NULL)
     Map<String, String> additional_headers;
+    @JsonInclude(NON_NULL)
+    List<SupportedCredential> credentials_supported;
     @Expose
     @JsonInclude(NON_NULL)
     @NotBlank
     String enabled;
+}
+
+
+@Data
+class SupportedCredential {
+    @NotEmpty
+    String format;
+    Map<String, Object> credential_definition;
+    String scope;
+    @JsonInclude(NON_NULL)
+    List<Object> display;
 }
