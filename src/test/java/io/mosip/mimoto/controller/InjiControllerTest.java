843da92fb32ec7e5a659565c89b95be86c51a855
Alka Prasad <prasadalka1998@gmail.com>
2024-06-17 17:18:04 +0530

    [INJIMOB-857]: remove autowire annotation from the controller class in test
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- src/test/java/io/mosip/mimoto/controller/InjiControllerTest.java
+++ src/test/java/io/mosip/mimoto/controller/InjiControllerTest.java
@@ -35,7 +35,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -59,10 +58,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @PrepareForTest({Files.class})
 public class InjiControllerTest {
 
-    @Autowired
     private CommonInjiController commonInjiController = new CommonInjiController();
 
-    @Autowired
     private CredentialShareController credentialShareController = new CredentialShareController();
 
     @MockBean
