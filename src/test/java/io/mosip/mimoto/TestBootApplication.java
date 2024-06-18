888c782548c6d0e9635e7393a95a2977e5b8f570
Alka Prasad <prasadalka1998@gmail.com>
2024-06-18 10:02:36 +0530

    [INJIMOB-857]: remove unused import
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- src/test/java/io/mosip/mimoto/TestBootApplication.java
+++ src/test/java/io/mosip/mimoto/TestBootApplication.java
@@ -2,7 +2,6 @@ package io.mosip.mimoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication(scanBasePackages = "io.mosip.mimoto.*")
 public class TestBootApplication {
