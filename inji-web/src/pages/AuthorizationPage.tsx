a054feaa5e5f1b0e29c27c9d5d44745b8c21b3b1
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-08-29 10:10:19 +0530

    [INJIWEB-724] : update error message for invalid client and redirect for OVP flow
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- inji-web/src/pages/AuthorizationPage.tsx
+++ inji-web/src/pages/AuthorizationPage.tsx
@@ -3,20 +3,34 @@ import {SpinningLoader} from "../components/Common/SpinningLoader";
 import {api} from "../utils/api";
 import {LandingPageWrapper} from "../components/Common/LandingPageWrapper";
 import {ErrorSheildIcon} from "../components/Common/ErrorSheildIcon";
+import {useTranslation} from "react-i18next";
 
 export const AuthorizationPage: React.FC = () => {
     const url = window.location.href;
     const currentQueryParams = new URLSearchParams(window.location.search);
     const [error, setError] = useState<String>(currentQueryParams.get("error")+"");
-    const [errorDesc, setErrorDesc] = useState<String>(currentQueryParams.get("error_description")+"");
+    const {t} = useTranslation("AuthorizationPage");
+
     useEffect( () => {
         if(url.indexOf("error") === -1) {
             window.location.href = api.mimotoHost + "/authorize" + window.location.search;
         }
     },[])
 
+
+    const getErrorCode = (errorCode: String) => {
+        return t(`error.code.${errorCode}`);
+    }
+
+    const getErrorMessage = (errorCode: String) => {
+        return t(`error.message.${errorCode}`);
+    }
+
+
     if(url.indexOf("error") == -1){
         return <div><LandingPageWrapper icon={<SpinningLoader/>} title={""} subTitle={""} gotoHome={false}/></div>
     }
-    return <div><LandingPageWrapper icon={<ErrorSheildIcon />} title={error+""} subTitle={errorDesc+""} gotoHome={false}/></div>
+    return <div><LandingPageWrapper icon={<ErrorSheildIcon />} title={getErrorCode(error)} subTitle={getErrorMessage(error)} gotoHome={false}/></div>
 }
+
+
