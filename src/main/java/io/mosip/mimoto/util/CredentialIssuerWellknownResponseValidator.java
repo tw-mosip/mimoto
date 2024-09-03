f2041bcef0f87994a2de28b0f0b87c744d88ced0
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-09-03 17:55:07 +0530

    [INJIMOB-1078] use builtin functions for performing equality check and map iteration
    Signed-off-by: KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/util/CredentialIssuerWellknownResponseValidator.java
+++ src/main/java/io/mosip/mimoto/util/CredentialIssuerWellknownResponseValidator.java
@@ -8,6 +8,7 @@ import io.mosip.mimoto.exception.InvalidWellknownResponseException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Map;
 import java.util.Set;
@@ -23,19 +24,22 @@ public class CredentialIssuerWellknownResponseValidator {
             throw new InvalidWellknownResponseException(sb.toString());
         }
 
-        for(Map.Entry<String, CredentialsSupportedResponse> entry: response.getCredentialConfigurationsSupported().entrySet()){
+        for (Map.Entry<String, CredentialsSupportedResponse> entry : response.getCredentialConfigurationsSupported().entrySet()) {
             CredentialsSupportedResponse supportedCredentialConfiguration = entry.getValue();
             //TODO: Extract the vc specific validations to separate classes
             if (supportedCredentialConfiguration.getFormat().equals("mso_mdoc")) {
                 if (StringUtils.isBlank(supportedCredentialConfiguration.getDoctype())) {
                     throw new InvalidWellknownResponseException("Mandatory field 'doctype' missing");
                 }
-                if(supportedCredentialConfiguration.getClaims().isEmpty() || supportedCredentialConfiguration.getClaims()==null){
+                if (CollectionUtils.isEmpty(supportedCredentialConfiguration.getClaims())) {
                     throw new InvalidWellknownResponseException("Mandatory field 'claims' missing");
                 }
             }
 
             if (supportedCredentialConfiguration.getFormat().equals("ldp_vc")) {
+                if (supportedCredentialConfiguration.getCredentialDefinition() == null) {
+                    throw new InvalidWellknownResponseException("credentialDefinition: must not be null");
+                }
                 Set<ConstraintViolation<CredentialDefinitionResponseDto>> credentialDefinitionViolations = validator.validate(supportedCredentialConfiguration.getCredentialDefinition());
                 if (!credentialDefinitionViolations.isEmpty()) {
                     StringBuilder sb = new StringBuilder("Validation failed:");
