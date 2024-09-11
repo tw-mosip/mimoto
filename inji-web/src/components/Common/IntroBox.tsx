76dceb28babb1a95be737ff4d21c0680e0031770
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-09-11 12:41:56 +0530

    [INJIWEB-268] : handle new UI enhancement.
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- inji-web/src/components/Common/IntroBox.tsx
+++ inji-web/src/components/Common/IntroBox.tsx
@@ -4,7 +4,7 @@ import {useTranslation} from "react-i18next";
 export const IntroBox: React.FC = () => {
     const {t} = useTranslation("HomePage");
     return <React.Fragment>
-        <div data-testid="IntroBox-Container" className="text-center pt-20 pb-20">
+        <div data-testid="IntroBox-Container" className="text-center pt-20 pb-10">
             <h2 data-testid="IntroBox-Text"
                 className="text-3xl text-iw-title ">{t("Intro.title")}</h2>
             <p data-testid="IntroBox-SubText"
--- inji-web/src/components/Common/ItemBox.tsx
+++ inji-web/src/components/Common/ItemBox.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import {ItemBoxProps} from "../../types/components";
 
 export const ItemBox: React.FC<ItemBoxProps> = (props) => {
+    console.log(`Index -> ${props.index}`)
+    console.log(`Mod Index -> ${props.index%6}`)
     return <React.Fragment>
         <div key={props.index}
              data-testid={`ItemBox-Outer-Container-${props.index}`}
-             className="bg-iw-tileBackground shadow flex flex-row shadow-iw-shadow p-4 rounded-md cursor-pointer items-center"
+             className={`bg-iw-tileBackground w-48 h-48 flex flex-col shadow hover:shadow-lg hover:scale-110 hover:shadow-iw-selectedShadow p-5 m-4 rounded-md cursor-pointer items-center`}
              onClick={props.onClick}
              onKeyUp={props.onClick}
              tabIndex={0}
              role="menuitem">
             <img data-testid="ItemBox-Logo" src={props.url} alt="Issuer Logo"
-                 className="w-30 h-10 justify-center me-4"/>
-            <div className={"justify-center items-center"}>
-                <h3 className="text-lg font-semibold text-iw-title"
+                 className="w-20 h-20 items-center justify-center me-4 flex"/>
+            <div className={"flex justify-center items-center pt-4"}>
+                <h3 className="flex text-sm font-semibold text-iw-title text-center"
                     data-testid="ItemBox-Text">{props.title}</h3>
-                {props.description && <p className="text-iw-title">{props.description}</p>}
             </div>
         </div>
     </React.Fragment>
