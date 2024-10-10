fd8b16cab19985420aa90fcf64947c33877dc34a
Mohanachandran S <165888272+mohanachandran-s@users.noreply.github.com>
2024-10-10 17:15:02 +0530

    MOSIP-36455 - Merge release 0.14.x branch with develop (#482)
    * MOSIP-36455 - Merge release 0.14.x branch with develop
    
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    
    * MOSIP-36455 - Merge release 0.14.x branch with develop
    
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    
    ---------
    
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/utils/MimotoUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/utils/MimotoUtil.java
@@ -1,9 +1,11 @@
 package io.mosip.testrig.apirig.utils;
 
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 import org.testng.SkipException;
 
 import io.mosip.testrig.apirig.dto.TestCaseDTO;
+import io.mosip.testrig.apirig.testrunner.OTPListener;
 
 public class MimotoUtil extends AdminTestUtil {
 
@@ -39,7 +41,6 @@ public class MimotoUtil extends AdminTestUtil {
 		String endpoint = testCaseDTO.getEndPoint();
 		String inputJson = testCaseDTO.getInput();
 		
-		
 		if (isOTPEnabled().equals("false")) {
 			if (testCaseDTO.getEndPoint().contains(GlobalConstants.SEND_OTP_ENDPOINT)
 					|| testCaseDTO.getInput().contains(GlobalConstants.SEND_OTP_ENDPOINT)
@@ -63,4 +64,55 @@ public class MimotoUtil extends AdminTestUtil {
 		return testCaseDTO;
 	}
 	
+	public static String getOTPFromSMTP(String inputJson, TestCaseDTO testCaseDTO) {
+		String testCaseName = testCaseDTO.getTestCaseName();
+		JSONObject request = new JSONObject(inputJson);
+		String emailId = null;
+		String otp = null;
+		
+		if (testCaseName.contains("ESignet_AuthenticateUser") && request.has(GlobalConstants.REQUEST)) {
+			if (request.getJSONObject(GlobalConstants.REQUEST).has(GlobalConstants.CHALLENGELIST)) {
+				if (request.getJSONObject(GlobalConstants.REQUEST).getJSONArray(GlobalConstants.CHALLENGELIST)
+						.length() > 0) {
+					if (request.getJSONObject(GlobalConstants.REQUEST).getJSONArray(GlobalConstants.CHALLENGELIST)
+							.getJSONObject(0).has(GlobalConstants.CHALLENGE)) {
+						if (request.getJSONObject(GlobalConstants.REQUEST).getJSONArray(GlobalConstants.CHALLENGELIST)
+								.getJSONObject(0).getString(GlobalConstants.CHALLENGE)
+								.endsWith(GlobalConstants.MAILINATOR_COM)
+								|| request.getJSONObject(GlobalConstants.REQUEST)
+										.getJSONArray(GlobalConstants.CHALLENGELIST).getJSONObject(0)
+										.getString(GlobalConstants.CHALLENGE).endsWith(GlobalConstants.MOSIP_NET)
+								|| request.getJSONObject(GlobalConstants.REQUEST)
+										.getJSONArray(GlobalConstants.CHALLENGELIST).getJSONObject(0)
+										.getString(GlobalConstants.CHALLENGE).endsWith(GlobalConstants.OTP_AS_PHONE)) {
+							emailId = request.getJSONObject(GlobalConstants.REQUEST)
+									.getJSONArray(GlobalConstants.CHALLENGELIST).getJSONObject(0)
+									.getString(GlobalConstants.CHALLENGE);
+							if (emailId.endsWith(GlobalConstants.OTP_AS_PHONE))
+								emailId = emailId.replace(GlobalConstants.OTP_AS_PHONE, "");
+							logger.info(emailId);
+							otp = OTPListener.getOtp(emailId);
+							request.getJSONObject(GlobalConstants.REQUEST).getJSONArray(GlobalConstants.CHALLENGELIST)
+									.getJSONObject(0).put(GlobalConstants.CHALLENGE, otp);
+							inputJson = request.toString();
+							return inputJson;
+						}
+					}
+				}
+			}
+		}
+		
+		return inputJson;
+	}
+	
+	public static String getClientIdSection(String baseURL) {
+		if (baseURL.contains("esignet-mosipid")) {
+			return "mimoto.oidc.mosipid.partner.clientid";
+		}
+		if (baseURL.contains("esignet-insurance")) {
+			return "mimoto.oidc.sunbird.partner.clientid";
+		}
+
+		return "mimoto.oidc.partner.clientid";
+	}
 }
