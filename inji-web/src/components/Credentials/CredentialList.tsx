5b84f8a3188051b798271c81a78ebeb1188d5c5c
Sairam Girirao <105053210+Sairam-g9162@users.noreply.github.com>
2024-10-24 14:59:47 +0530

    [INJIWEB-1001]:Fix Mobile View Overlap in Credential List (#171)
    Signed-off-by: SAIRAM GIRIRAO <sairam.girirao@infosys.com>

--- inji-web/src/components/Credentials/CredentialList.tsx
+++ inji-web/src/components/Credentials/CredentialList.tsx
@@ -31,7 +31,7 @@ export const CredentialList: React.FC<CredentialListProps> = ({state}) => {
 
     return <React.Fragment>
         <HeaderTile content={t("containerHeading")} subContent={t("containerSubHeading")}/>
-        <div className="grid grid-cols-8 gap-6 md:grid-cols-6 sm:grid-cols-4">
+        <div className="flex flex-wrap gap-3 p-4 pb-20 justify-start">
             {credentials?.filtered_credentials && Object.keys(credentials?.filtered_credentials?.credential_configurations_supported).map((credentialId: string, index: number) => (
                 <Credential credentialId={credentialId} credentialWellknown={credentials?.filtered_credentials} key={index} index={index}/>
             ))}
