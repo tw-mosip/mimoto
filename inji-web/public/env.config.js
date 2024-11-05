cc95d13906aa5b2f39b6a8b151c107401eec89f4
Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>
2024-11-05 19:08:27 +0530

    [INJIWEB-1064]: Fix Title (#186)
    * [INJIWEB-1064]: Fix Title
    
    Signed-off-by: Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>
    
    * [INJIWEB-1064]: Fix Title
    
    Signed-off-by: Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>
    
    ---------
    
    Signed-off-by: Kamlesh Singh Bisht <kamlesh.bisht@infosys.com>

--- inji-web/public/env.config.js
+++ inji-web/public/env.config.js
@@ -2,7 +2,7 @@ window._env_ = {
     DEFAULT_LANG: "en",
     DEFAULT_THEME: "",
     DEFAULT_FAVICON: "favicon.ico",
-    DEFAULT_TITLE: "Inji Web",
+    DEFAULT_TITLE: "Inji Wallet",
     DEFAULT_FONT_URL: "https://fonts.googleapis.com/css?family=Inter",
     MIMOTO_HOST: "http://localhost:3010"
 };
