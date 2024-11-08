33dafd020cad2886c94b572227d7a6555807bfb4
Vijay Kumar S <94220135+vijay151096@users.noreply.github.com>
2024-11-08 11:59:36 +0530

    [INJIWEB-990]: verify ed25519Signature2018 and RSASignature2018 verifiable credential (#507)
    * [INJIWEB-990]: verify sunbird vc
    
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>
    
    * [INJIWEB-990]: mock vc
    
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>
    
    * [INJIWEB-990]: modify the responseType for exception
    
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>
    
    * [INJIWEB-990]: remove unwated snippet which is added for testing
    
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/service/CredentialService.java
+++ src/main/java/io/mosip/mimoto/service/CredentialService.java
@@ -1,13 +1,6 @@
 package io.mosip.mimoto.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.mosip.mimoto.dto.idp.TokenResponseDTO;
-import io.mosip.mimoto.dto.mimoto.VCCredentialResponse;
 import io.mosip.mimoto.exception.ApiNotAccessibleException;
-import io.mosip.vercred.exception.ProofDocumentNotFoundException;
-import io.mosip.vercred.exception.ProofTypeNotSupportedException;
-import io.mosip.vercred.exception.SignatureVerificationException;
-import io.mosip.vercred.exception.UnknownException;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
