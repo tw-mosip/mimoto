0a3331cf5887149d211e0bb9a30757c9bd4ee787
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-02-22 17:17:38 +0530

    [INJIWEB-1370] add access_type & prompt query params to google authorization url and make changes to store the access token & refresh token in session
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/io/mosip/mimoto/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -18,14 +18,17 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Autowired
     private UserMetadataService userMetadataService;
 
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) authentication;
         OAuth2User oAuth2User = oauth2Token.getPrincipal();
-
+        HttpSession session = request.getSession();
         // Storing clientRegistrationId in the Redis session to verify it against the one in user metadata during profile retrieval
         String clientRegistrationId = oauth2Token.getAuthorizedClientRegistrationId();
-        request.getSession().setAttribute("clientRegistrationId", clientRegistrationId);
+        session.setAttribute("clientRegistrationId", clientRegistrationId);
 
         // Extracting OAuth2 user information
         String providerSubjectId = oAuth2User.getAttribute("sub");
@@ -33,6 +36,14 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         String displayName = oAuth2User.getAttribute("name");
         String profilePictureUrl = oAuth2User.getAttribute("picture");
         String email = oAuth2User.getAttribute("email");
+
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService
+                .loadAuthorizedClient(oauth2Token.getAuthorizedClientRegistrationId(), authentication.getName());
+
+        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+        String refreshToken = authorizedClient.getRefreshToken().getTokenValue();
+        session.setAttribute("accessToken", accessToken);
+        session.setAttribute("refreshToken", refreshToken);
 
         // Call the service to update or insert the user metadata in the database
         userMetadataService.updateOrInsertUserMetadata(providerSubjectId, identityProvider, displayName, profilePictureUrl, email);
