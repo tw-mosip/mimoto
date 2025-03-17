30fbced861867f62afc4effa7f34a03ac7b5f10e
Gurpreet41082 <gurpreet.kaur@thoughtworks.com>
2025-03-17 18:08:08 +0530

    [injiweb-1392] added api for wallet
    Signed-off-by: Gurpreet41082 <gurpreet.kaur@thoughtworks.com>

--- src/main/java/io/mosip/mimoto/controller/UserController.java
+++ src/main/java/io/mosip/mimoto/controller/UserController.java
@@ -3,20 +3,26 @@ package io.mosip.mimoto.controller;
 import io.mosip.mimoto.core.http.ResponseWrapper;
 import io.mosip.mimoto.dbentity.UserMetadata;
 import io.mosip.mimoto.dto.mimoto.UserMetadataDTO;
+import io.mosip.mimoto.dto.WalletRequestDto;
 import io.mosip.mimoto.exception.OAuth2AuthenticationException;
 import io.mosip.mimoto.repository.UserMetadataRepository;
+import io.mosip.mimoto.service.WalletService;
 import io.mosip.mimoto.util.EncryptionDecryptionUtil;
 import io.mosip.mimoto.util.Utilities;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessResourceFailureException;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static io.mosip.mimoto.exception.PlatformErrorMessages.*;
 
 @Slf4j
@@ -29,6 +35,9 @@ public class UserController {
 
     @Autowired
     private EncryptionDecryptionUtil encryptionDecryptionUtill;
+
+    @Autowired
+    private WalletService walletService;
 
     @GetMapping("/profile")
     public ResponseEntity<ResponseWrapper<UserMetadataDTO>> getUserProfile(Authentication authentication, HttpSession session) {
@@ -62,5 +71,47 @@ public class UserController {
     private UserMetadata fetchUserMetadata(String providerSubjectId, String identityProvider) throws OAuth2AuthenticationException {
         return userMetadataRepository.findByProviderSubjectIdAndIdentityProvider(providerSubjectId, identityProvider)
                 .orElseThrow(() -> new OAuth2AuthenticationException(USER_METADATA_FETCH_EXCEPTION.getCode(), "User not found. Please check your credentials or register", HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/wallets")
+    public ResponseEntity<ResponseWrapper<UUID>> createWallet(@RequestBody WalletRequestDto wallet, HttpSession httpSession) {
+        try {
+            ResponseWrapper<UUID> responseWrapper = new ResponseWrapper<>();
+            responseWrapper.setResponse(walletService.createWallet((UUID) httpSession.getAttribute("userId"), wallet.getPin()));
+            return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
+        } catch (Exception exception) {
+            log.error("Error occurred while creating user wallets : ", exception);
+            return Utilities.handleErrorResponse(exception, USER_WALLET_CREATION_EXCEPTION.getCode(), HttpStatus.INTERNAL_SERVER_ERROR,MediaType.APPLICATION_JSON);
+        }
+    }
+
+    @GetMapping("/wallets")
+    public ResponseEntity<ResponseWrapper<List<UUID>>> getWallets(HttpSession httpSession) {
+        try {
+            ResponseWrapper<List<UUID>> responseWrapper = new ResponseWrapper<>();
+            List<UUID> list = new ArrayList<>();
+            list.addAll(walletService.getWallets((UUID) httpSession.getAttribute("userId")));
+            responseWrapper.setResponse(list);
+            return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
+        } catch (Exception exception) {
+            log.error("Error occurred while retrieving user wallets : ", exception);
+            return Utilities.handleErrorResponse(exception, USER_WALLET_RETRIEVAL_EXCEPTION.getCode(), HttpStatus.INTERNAL_SERVER_ERROR,MediaType.APPLICATION_JSON);
+        }
+    }
+
+    @PostMapping("/wallets/{walletId}")
+    public ResponseEntity<ResponseWrapper<UUID>> getWallet(@PathVariable("walletId") UUID walletId, @RequestBody WalletRequestDto wallet, HttpSession httpSession) {
+        try {
+            // If wallet_key does not exist in the session, fetch it and set it in the session
+            String walletKey = walletService.getWalletKey((UUID) httpSession.getAttribute("userId"), walletId, wallet.getPin());
+            httpSession.setAttribute("wallet_key", walletKey);
+
+            ResponseWrapper<UUID> responseWrapper = new ResponseWrapper<>();
+            responseWrapper.setResponse(walletId);
+            return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
+        } catch (Exception exception) {
+            log.error("Error occurred while retrieving user wallet ", exception);
+            return Utilities.handleErrorResponse(exception, USER_WALLET_RETRIEVAL_EXCEPTION.getCode(), HttpStatus.INTERNAL_SERVER_ERROR,MediaType.APPLICATION_JSON);
+        }
     }
 }
