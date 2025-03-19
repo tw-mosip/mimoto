8526fdbdccc9aee3699bd3dc897565b32afabfd1
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-03-19 09:47:55 +0530

    [INJIWEB-1370] handle the connection and timeout scenarios during connecting with the IDP flow
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/security/oauth2/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/io/mosip/mimoto/security/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -3,7 +3,9 @@ package io.mosip.mimoto.security.oauth2;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -15,20 +17,33 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+    @Value("${mosip.inji.web.url}")
+    private String injiWebUrl;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
             throws IOException {
-        String error = request.getParameter("error");
-        String errorMessage;
+        String errorMessage = "";
 
-        if ("access_denied".equals(error)) {
-            errorMessage = "Access Denied. Please try again.";
-        } else {
-            errorMessage = "Authentication failed. Please try again.";
+        if (exception != null) {
+            if (exception instanceof OAuth2AuthenticationException) {
+                Throwable cause = exception.getCause();
+                if (cause instanceof java.net.SocketTimeoutException) {
+                    errorMessage = "Timeout while connecting to the IDP for authorization.";
+                } else if (cause instanceof java.net.ConnectException) {
+                    errorMessage = "Could not connect to the IDP for authorization.";
+                } else if (exception.getMessage().contains("access_denied")) {
+                    errorMessage = "Access Denied. Please try again.";
+                } else {
+                    errorMessage = "IDP login is failed due to: " + exception.getMessage();
+                }
+            } else {
+                errorMessage = "IDP login is failed due to: " + exception.getMessage();
+            }
         }
 
         String encodedErrorMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
-        String redirectUrl = "http://localhost:3004/login?status=error&error_message=" + encodedErrorMessage;
+        String redirectUrl = injiWebUrl + "/login?status=error&error_message=" + encodedErrorMessage;
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 }
