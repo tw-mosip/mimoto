4a17c68f4ebfd3b4cfd5bc2d66188503c450b0e0
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-07-09 09:51:55 +0530

    [INJIWEB-1607] add: traceId in the logs
    Passing micrometer tracer to tomcat factory to attach tracer info.
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- src/main/java/io/mosip/mimoto/config/AppConfig.java
+++ src/main/java/io/mosip/mimoto/config/AppConfig.java
@@ -1,10 +1,15 @@
 package io.mosip.mimoto.config;
 
 
+import io.mosip.kernel.core.logger.config.SleuthValve;
 import io.mosip.pixelpass.PixelPass;
 import io.mosip.vercred.vcverifier.CredentialsVerifier;
+import org.apache.catalina.Valve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.micrometer.tracing.Tracer;
 
 @Configuration
 public class AppConfig {
@@ -16,5 +21,13 @@ public class AppConfig {
     @Bean
     public PixelPass pixelPass() {
         return new PixelPass();
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> sleuthValveCustomizer(Tracer tracer) {
+        return factory -> {
+            Valve sleuthValve = new SleuthValve(tracer);
+            factory.addContextValves(sleuthValve);
+        };
     }
 }
